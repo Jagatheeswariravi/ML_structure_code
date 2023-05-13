89b49851c9850410404bb331eae2f641d059e623
Jagatheeswari <jagavirat1010@gmail.com>
2023-05-13 10:38:49 +0530

    dataingestion

--- src/components/data_ingestiion.py
+++ src/components/data_ingestiion.py
@@ -5,17 +5,18 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-from dataclasses import dataclass
 
-@dataclass
+train_data_path = os.path.join("artifact","train.csv")
+test_data_path = os.path.join("artifact","test.csv")
+raw_data_path  = os.path.join("artifact","raw_data.csv")
 
-class DataIngestionConfig:
-    train_data_path:str = os.path.join("artifact","train.csv")
-    test_data_path:str= os.path.join("artifact","test.csv")
-    raw_data_path:str  = os.path.join("artifact","raw_data.csv")
+
 class DataIngestion:
-    def __init__(self):
-        self.ingestion_config = DataIngestionConfig()
+    def __init__(self,train_data_path,test_data_path,raw_data_path):
+        self.train_data_path = train_data_path
+        self.test_data_path = test_data_path
+        self.raw_data_path  = raw_data_path
+        
     
     def intiate_data_ingestion(self):
         logging.info("Entered to the data collection class")
@@ -25,24 +26,24 @@ class DataIngestion:
         try:
             df = pd.read_csv("C:\\Users\\jagar\\codestruc\\data\\StudentsPerformance.csv") 
 
-            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+            os.makedirs(os.path.dirname(self.train_data_path),exist_ok=True)
 
-            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+            df.to_csv(self.raw_data_path,index=False,header=True)
 
             logging.info("train_test is initiated")
 
             train_set,test_set = train_test_split(df,test_size=0.2,random_state=102)
 
-            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            train_set.to_csv(self.train_data_path,index=False,header=True)
 
-            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+            test_set.to_csv(self.test_data_path,index=False,header=True)
 
             logging.info("train_test split is finished")
         
             return (
-            self.ingestion_config.raw_data_path,
-            self.ingestion_config.train_data_path,
-            self.ingestion_config.test_data_path
+            self.raw_data_path,
+            self.train_data_path,
+            self.test_data_path
         
         )
 
